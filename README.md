3b7b3d2163c04b3ee7206a7cdd1cd4fb108c193d
Rachael Creager <rccreager@gmail.com>
2017-07-06 12:13:07 -0400

    adding root_to_hdf5 tools

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # HLT-btag-studies
 directory containing tools for studying b-tagging with HLT values vs offline
+
+setup.sh: point to whatever local install of root you want
---
+++ setup.sh
@@ -0,0 +1,13 @@
+
+#change root to the version installed with brew
+source $(brew --prefix root6)/libexec/thisroot.sh
+
+
+#set your library path var
+#export LD_LIBRARY_PATH=$ROOTSYS/lib:$LD_LIBRARY_PATH
+
+#source my_project/bin/activate
+#export LD_LIBRARY_PATH=/usr/local/Cellar/python/2.7.11/Frameworks/Python.framework/Versions/2.7/lib:$LD_LIBRARY_PATH
+#export LD_LIBRARY_PATH=/Users/rcreager/Dropbox/Documents/bTagging_and_ML/root_to_hdf5/my_project/bin/python/
+
+
